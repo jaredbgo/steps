45b2f828d1cf2767bddd97f49909385b08c2bce3
jgoldberg <jgoldberg@consumer-edge.com>
2021-09-16 14:42:36 -0400

    adding sidebar

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,20 +2,20 @@ import pandas as pd
 import streamlit as st
 import altair as alt
 
-st.title('Jared likes to walk!')
-st.write('## Explore his adventures below\n')
-st.write('')
+sideb = st.sidebar
+
+sideb.title('Jared likes to walk!')
+sideb.write('Explore his adventures below \n\n\n\n')
 
 miles = pd.read_csv('milage.csv').fillna('None')
 steps = pd.read_csv('steps.csv').fillna('None')
 
 print(miles)
 
-metric = st.radio("Steps or miles?", ('Steps', 'Miles'))
+metric = sideb.radio("Steps or miles?", ('Steps', 'Miles'))
 
-start_date = st.date_input("Pick a start date")
-end_date = st.date_input("Pick an end date")
-
+start_date = sideb.date_input("Pick a start date")
+end_date = sideb.date_input("Pick an end date")
 
 if metric == 'Miles':
 
@@ -25,11 +25,19 @@ else:
 
 
 if plotter.shape[0] == 0:
-	st.write('Oh no! We found no data for the supplied date range')
+	st.error('Oh no! We found no data for the supplied date range')
 else:
+	st.write('## {m} Walked from {s} to {e}'.format(m=metric, s=str(start_date), e=str(end_date)))
 	#st.bar_chart(plotter.set_index('Date'), columns= ['Miles', 'Event'])
 	#print(plotter.set_index('Date'))
 	#st.bar_chart(data=miles)
 
-	base = alt.Chart(plotter).mark_bar().encode(x='Date', y=metric, tooltip=['Date', metric, 'Event']).interactive().configure_axisX(labelAngle=-45) #labelAlign='center')
+	base = alt.Chart(plotter).mark_bar().encode(x='Date', y=metric, tooltip=['Date', metric, 'Event']).interactive().configure_axisX(labelAngle=-45, labelOverlap=True) #labelAlign='center')
 	st.altair_chart(base, use_container_width=True)
+
+
+	# Adding maxumimum
+
+	max_value = plotter[metric].max()
+
+	
